46d1d51bbf4d6a82c526aa4f8ecee36aa6601aba
Qubemap <qubemaps@gmail.com>
2024-07-17 16:33:41 +0930

    can'tfindrotate

--- Tanks/TankPlayer.cpp
+++ Tanks/TankPlayer.cpp
@@ -3,9 +3,12 @@
 void TankPlayer::OnUpdate(float deltaTime)
 {
 	const float MOVESPEED = 150.0f;
+	const float ROTSPEED = (1 * DEG2RAD);
 
 	// check for key input
 	float yMove = 0.0f;
+
+	float rot = 0.0f;
 
 	//W-S for UP-Down movement
 	if (IsKeyDown(KeyboardKey::KEY_W))
@@ -17,8 +20,21 @@ void TankPlayer::OnUpdate(float deltaTime)
 		yMove -= MOVESPEED;
 	}
 
+	if (IsKeyDown(KeyboardKey::KEY_Q))
+	{
+		rot += ROTSPEED;
+	}
+	if (IsKeyDown(KeyboardKey::KEY_E))
+	{
+		rot -= ROTSPEED;
+	}
+
 	// displacement for this frame
 	MathClasses::Vector3 finalMove = GetForward() * (yMove * deltaTime);
 
+	Rotate(rot);
+
 	Translate(finalMove);
+
+	
 }
